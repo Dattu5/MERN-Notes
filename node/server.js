94dcd9b6765815c0ef556b0913ef708c2866f293
Dattu5 <dattatreyagokhale@gmail.com>
2025-10-19 22:14:24 +0530

    Connect to MongoDB Atlas

--- node/server.js
+++ node/server.js
@@ -5,7 +5,26 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+///////////////
 
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const uri = process.env.MONGO_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected ✅"))
+  .catch(err => console.error("MongoDB connection error ❌", err));
+
+
+
+
+
+
+
+
+
+///////////////
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 let users; // ✅ declare here
