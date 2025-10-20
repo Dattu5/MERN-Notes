151d468bcca8fc5a305d217d10011f4e828258a2
Dattu5 <dattatreyagokhale@gmail.com>
2025-10-20 07:54:40 +0530

    Initial commit

--- node/server.js
+++ node/server.js
@@ -5,16 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-///////////////
-
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-const uri = process.env.MONGO_URI;
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected ✅"))
-  .catch(err => console.error("MongoDB connection error ❌", err));
+ 
 
 
 
@@ -22,10 +13,7 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 
-
-
-///////////////
-const url = 'mongodb://localhost:27017';
+ const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 let users; // ✅ declare here
 
